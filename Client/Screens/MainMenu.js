c39f104664e41d55df4117784dfe8326a8cef06f
KennethChong <kenneth.chong@sjsu.edu>
2019-03-22 15:27:00 -0700

    Change the tab screen

--- Client/Screens/MainMenu.js
+++ Client/Screens/MainMenu.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { createDrawerNavigator, createAppContainer} from 'react-navigation'
+import { createDrawerNavigator, createBottomTabNavigator, createAppContainer} from 'react-navigation'
 
 import HomeScreen from "./HomeScreen";
 import ProfileScreen from "./ProfileScreen";
@@ -10,46 +10,20 @@ class MainMenu extends React.Component {
     render() {
         return (
            <AppMainContainer
-                // ref={navigatorRef => {
-                //     NavigationService.setTopLevelNavigator(navigatorRef)
-                // }}
             />
         );
     }
 }
 
 //Main Navigation of the Application 
-const AppMainNavigator = createDrawerNavigator(
+const AppMainNavigator = createBottomTabNavigator(
     {
         'Home': HomeScreen,
         'Profile': ProfileScreen,
         'About': AboutScreen,
     },
-    // {
-    //     contentComponent: customMainComponent,
-    //     contentOptions: {
-    //         activeTintColor: '#2196f3'
-    //     }
-    // }
 )
 
 const AppMainContainer = createAppContainer(AppMainNavigator);
-
-// const customMainComponent = props => 
-// (
-//     <SafeAreaView style = {{flex:1}}> 
-//         <View>
-//             style = {{
-//                 height: 150,
-//                 backgroundColor: 'Green',
-//                 alignItems: 'center',
-//                 justifyContent: 'center',
-//             }}
-//         </View>
-//         <ScrollView>
-//             <DrawerItems {...props} />
-//         </ScrollView>
-//     </SafeAreaView>
-// )
 
 export default MainMenu 
