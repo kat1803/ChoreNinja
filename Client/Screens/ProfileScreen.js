682fbb8ca979acf0411e4363bba4d37ba2b578bc
Kat1803 <kathy.luu0909@gmail.com>
2019-07-16 20:46:33 -0700

    started the pro5

--- Client/Screens/ProfileScreen.js
+++ Client/Screens/ProfileScreen.js
@@ -1,16 +1,53 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import {StyleSheet,Image,TouchableOpacity} from "react-native";
+
 
 class ProfileScreen  extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <Text>Profile Screen</Text>
+          
+<View>
+            <View style={styles.Header}>
+                    <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+                        Master Profile
+                    </Text>
+                    <Image style={{ width: 40, height: 40 }}
+                        source={require("../assets/crow.png")}/>
             </View>
-        );
-    }
-}
+            <View style={styles.Body}>
+                    <Image style={{ width: 100, height: 100, overflow: 'hidden', borderRadius: 80 }}
+                        source={require('../assets/snoopdogg.jpg')} />
+                        <Text style={{fontWeight: "bold", fontSize: 25 }}>Snoop Dogg</Text>
+            </View>
+            <View style={styles.Body}>
+                <Text>Address: Hollywood</Text>
+
+            </View>
+</View>
+          );
+        }
+      }
+      
+      const styles = StyleSheet.create({
+        Header: {
+            marginTop: "5%",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            borderBottomWidth: 1,
+            borderBottomColor: "#D3D3D3"
+          },
+        Body: {
+            flexDirection: 'column', 
+            alignItems: "center", 
+            justifyContent: "center", 
+            flexDirection: 'column',
+            borderBottomColor: "#D3D3D3"
+        }  
+       
+      });
+
 
 
 export default ProfileScreen
