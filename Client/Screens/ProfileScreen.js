8b45ed1ac32eaec4b6ac4268fa5237b5d7cb480a
Kat1803 <kathy.luu0909@gmail.com>
2019-07-21 21:21:50 -0700

    Finished the pro5 customer
    Did the customer profile and the about screen.

--- Client/Screens/ProfileScreen.js
+++ Client/Screens/ProfileScreen.js
@@ -1,70 +1,85 @@
 import React from "react";
 import { View, Text } from "react-native";
-import {StyleSheet, Image, FlatList, AppRegistry, Component, TouchableOpacity} from "react-native";
+import { Card, ListItem, Button, Icon } from "react-native-elements";
+import {
+  StyleSheet,
+  Image,
+  FlatList,
+  AppRegistry,
+  Component,
+  TouchableOpacity
+} from "react-native";
+import { Divider } from 'react-native-elements';
+
+// Formatted with the following command:
+// Nhis-MacBook-Pro:Screens kathryn$ prettier --write ProfileScreen.js
 
 
-class ProfileScreen  extends React.Component {
-    render() {
-        return (
-          
+class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={styles.Header}>
+          <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+            Master Profile
+          </Text>
+          <Image
+            style={{ width: 40, height: 40 }}
+            source={require("../assets/crow.png")}
+          />
+        </View>
+        <View style={styles.Body}>
+          <Image
+            style={{
+              width: 100,
+              height: 100,
+              overflow: "hidden",
+              borderRadius: 80
+            }}
+            source={require("../assets/snoopdogg.jpg")}
+          />
+          <Text style={{ fontWeight: "bold", fontSize: 25 }}>Snoop Dogg</Text>
+        </View>
+
 <View>
-            <View style={styles.Header}>
-                    <Text style={{ fontWeight: "bold", fontSize: 15 }}>
-                        Master Profile
-                    </Text>
-                    <Image style={{ width: 40, height: 40 }}
-                        source={require("../assets/crow.png")}/>
-            </View>
-            <View style={styles.Body}>
-                    <Image style={{ width: 100, height: 100, overflow: 'hidden', borderRadius: 80 }}
-                        source={require('../assets/snoopdogg.jpg')} />
-                        <Text style={{fontWeight: "bold", fontSize: 25 }}>Snoop Dogg</Text>
+        <Card >
+          <ListItem style={{borderBottomColor: "#000000"}} title="Address:"/>
 
-            </View>
-        
-            <View style={{  margin: 10,
-                                 borderRadius: 3,
-                                 borderWidth: 1,
-                                 borderColor: '#d6d7da',
-                                 flex: 3, justifyContent:'flex-start',alignItems: "flex-start", flexDirection:'column', padding: 5}}>
-                    <Text style={{fontWeight:'bold'}}>Bio:</Text><Text>I am software engineer student at SJSU. This is choreNinja, our senior project</Text>
-         
-                </View>
-</View>
-
-          );
-        }
-      }
-      
-      const styles = StyleSheet.create({
-        Header: {
-            marginTop: "5%",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            borderBottomWidth: 1,
-            borderBottomColor: "#D3D3D3"
-          },
-        Body: {
-            flexDirection: 'column', 
-            alignItems: "center", 
-            justifyContent: "center", 
-            flexDirection: 'column',
-            borderBottomColor: "#D3D3D3"
-        }  ,
-        container: {
-          flex: 1,
-          paddingTop: 22,
-          borderTopColor: "#696969"
-         },
-         item: {
-           padding: 10,
-           fontSize: 18,
-           height: 44,
-         },
-       
-      });
+          <ListItem title="Payment Type :"/>
+          <ListItem title="Email address:"/>
+          <ListItem title="Message :"/>
+          <ListItem title="Change Password :"/>
+          <ListItem title=""/>
+          <Button style={{alignItems: 'center', alignContent: 'center'}} title="Edit"></Button>
+        </Card>
+        <Button style={{alignItems: 'center', alignContent: 'center', paddingTop: 30}} title="Invite your friend to get $10 off" type="outline" />
+        <Button style={{alignItems: 'center', alignContent: 'center', paddingTop: 30}} title="Log out" type="outline" />
+      </View>
+      </View>
+    );
+  }
+}
 
 
+const styles = StyleSheet.create({
+  Header: {
+    marginTop: "5%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#D3D3D3"
+  },
+  Body: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    borderBottomColor: "#D3D3D3"
+  },
+ Info: {
+   borderBottomColor: "black"
+ }
+});
 
-export default ProfileScreen
+export default ProfileScreen;
