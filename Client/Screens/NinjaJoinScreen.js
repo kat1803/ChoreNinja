c30615b1ccae1b518850c8d614752ca2b3961396
KennethChong <kenneth.chong@sjsu.edu>
2019-07-28 09:50:41 -0700

    Update NinjaJoinScreen.js

--- Client/Screens/NinjaJoinScreen.js
+++ Client/Screens/NinjaJoinScreen.js
@@ -7,8 +7,11 @@ class NinjaJoinScreen extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <Text>Hey, are you interested to join as a ninja?</Text>
-                <Button mode="contained"> JOIN NOW </Button>
+                <View style={{alignContent: "space-between"}}>
+                    <Text>Hey, are you interested to join as a ninja?</Text>
+                    <Button mode="contained"> JOIN NOW </Button>
+                </View>
+                
             </View>
         );
     }
