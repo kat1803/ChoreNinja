4c309b5fcf38d00faa94abe51261e836cf964a6b
KennethChong <kenneth.chong@sjsu.edu>
2019-07-28 10:50:39 -0700

    Update NinjaJoinScreen.js

--- Client/Screens/NinjaJoinScreen.js
+++ Client/Screens/NinjaJoinScreen.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import { Button } from 'react-native-paper' ; 
+import { Button, Appbar } from 'react-native-paper' ; 
 
 class NinjaJoinScreen extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <View style={{alignContent: "space-between"}}>
-                    <Text>Hey, are you interested to join as a ninja?</Text>
-                    <Button mode="contained"> JOIN NOW </Button>
-                </View>
+            <View>
+                <Appbar.Header>
+                    <Appbar.Content
+                        title="Join as a NINJA!"
+                        style={{alignItems: 'center'}}
+                    />
+                    <Appbar.Action icon="notifications" onPress={() => console.log('Pressed')} />
+                    <Appbar.Action icon="inbox" onPress={() => console.log('Pressed')} />
+                </Appbar.Header>
+                <View style={{alignSelf: 'center', alignItems: 'center', justifyContent: 'center' }}>
+                    
+                        <Text>Hey, are you interested to join as a ninja?</Text>
+                        <Button mode="contained"> JOIN NOW </Button>
                 
+                </View>
             </View>
         );
     }
