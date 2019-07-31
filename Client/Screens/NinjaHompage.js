dd4c8134e97307f8220e788fbb8e6d8ffcfbff8e
KennethChong <kenneth.chong@sjsu.edu>
2019-07-30 19:07:33 -0700

    Install react native element

--- Client/Screens/NinjaHompage.js
+++ Client/Screens/NinjaHompage.js
@@ -39,7 +39,7 @@ class NinjaHomepage extends React.Component {
                         onPress={() => console.log('Pressed')}
                     />
                 </View>
-                <View>
+        
                 {this.state.Ninjas.map((n,i)=>{
                     return (
                         <Card key={i} flexDirection="row" >
@@ -64,7 +64,7 @@ class NinjaHomepage extends React.Component {
 
                     )
                 })}
-                </View>
+                
             </View>
         );
     }
