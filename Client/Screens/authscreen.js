20b6025c2c4b3b5fcf6d4dd9063930b9972a34be
KennethChong <kenneth.chong@sjsu.edu>
2019-08-21 13:57:24 -0700

    Create AuthScreen Testing

---
+++ Client/Screens/authscreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ScrollView, View, KeyboardAvoidingView, StyleSheet } from 'react-native';
+
+const Authscreen = props => {};
+
+const styles = StyleSheet.create({});
+
+export default Authscreen; 
