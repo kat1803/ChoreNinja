864d627a292d1f19f24bb509fe6707d360a4e3f9
KennethChong <kenneth.chong@sjsu.edu>
2019-08-24 11:31:17 -0700

    Install passport

--- Client/Screens/authscreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {ScrollView, View, KeyboardAvoidingView, StyleSheet } from 'react-native';
-
-const Authscreen = props => {};
-
-const styles = StyleSheet.create({});
-
-export default Authscreen; 
