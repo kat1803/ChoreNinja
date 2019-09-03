7d2cd823fdc08091dd5a9879fff5cac83f2782c0
KennethChong <kenneth.chong@sjsu.edu>
2019-09-03 11:27:25 -0700

    update controllers & app.js

--- Server/controllers/authentication.js
+++ Server/controllers/authentication.js
@@ -1,24 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+var isAuthenticated = function (req, res, next) {
+    //if the user is authenticated in the session, call the next() to call the request handler
+    if(req.isAuthenticated()) 
+        return next();
+    //if user not authenticated then redirect him to login page.
+    res.redirect('/');
+}
+
 module.exports = function(passport){
+    
+    //Get login page
     router.get('/', function(req,res){
         res.render('index', {message: req.flash('message')});
     });
     
+    //Handle login page
     router.post('/login', passport.authentication('login', {
         successRedirect: '/home',
         failuerRedirect: '/',
         failureFlash: true
     }));
 
+    //Get registration page
     router.get('/signup', function(req, res)
     {
         res.render('register', {message: req.flash('message')});
     });
 
+    //Handle registration post
     router.post('/signup', passport.authenticate('signup', {
         successRedirect: '/home',
         failuerRedirect: '/signup',
         failureFlash : true
     }));
+
+    //get homepage
+    router.get('/home', isAuthenticated, function(req, res){
+        res.render('home', {user: req.user});
+    });
+
+    //Handle Logout
+    router.get('/signout', function(req, res){
+        req.logout();
+        res.redirect('/');
+    })
 
     return router;
 }
--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 // Get all the module packages
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const job = require('./controllers/job.js')
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
+const job = require('./controllers/job.js');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 // Connect to database
 let db = require('./config/default.json')
@@ -24,6 +26,15 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Here is the API route
 app.use('/api/v1/job', job)
 
+//Importing modules for database
+var dbConfig = require('./db');
+var mongoose = require('mongoose');
+
+//Connect to DB for authentication
+mongoose.connect(dbConfig.url);
+
+var app = express();
+
 //Configure db.js with mongoose API 
 var dbConfig = require('./Authentication/db.js');
 var mongoose = require('mongoose');
@@ -35,5 +46,20 @@ var expressSession = require('express-session');
 app.use(expressSession({ secret: 'myChoreNinjaSecretKey' }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+//Initialize Passport
+var initPassport = require('./Authentication/init');
+initPassport(passport);
+
+var routes = require('./controllers/authentication')(passport);
+app.use('/', routes);
+
+//To catch 404 and forward to error handler
+app.use(function(req,res, next)
+{
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 module.exports = app
