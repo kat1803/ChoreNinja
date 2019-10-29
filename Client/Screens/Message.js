1b395a5e716de95dc3707d2f71e090c812e4f675
Phuc Le <phuc.le@ibm.com>
2019-10-28 20:36:56 -0700

    add chat functionality

--- Client/Screens/Message.js
+++ Client/Screens/Message.js
@@ -1,46 +1,67 @@
 // export default Message
-
+import firebaseService from '../Firebase/firebase'
 import React from 'react'
 import { GiftedChat } from 'react-native-gifted-chat'
+import { View, Text, Switch } from "react-native";
+import { List, Button } from 'react-native-paper';
 
 class Message extends React.Component {
   state = {
     messages: [],
+    conID : '',
+    conIDs: ['Conversation1', 'Conversation2']
   }
 
-  componentWillMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-      ],
-    })
+  changeConID = (conID) =>{
+    firebaseService.setConversation(conID)
+    this.setState({ conID, message: [] })
   }
 
-  onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+  componentDidMount() {
+    firebaseService.refOn(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
+    );
+  }
+  componentWillUnmount() {
+    firebaseService.refOff();
   }
 
   render() {
-    return (
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={messages => this.onSend(messages)}
-        user={{
-          _id: 1,
-        }}
-      />
-    )
+    const { conID, conIDs } = this.state
+    console.log(conID, conIDs)
+
+      if (conID != ''){
+        return (
+          <View style={{ flex: 1 }}>
+            <Button mode="contained" onPress={() => this.changeConID('')}>
+              Back
+            </Button>
+            <GiftedChat
+              messages={this.state.messages}
+              onSend={firebaseService.send}
+              user={{
+                _id: 1,
+              }}
+            />
+          </View>)
+      } else{
+        return (
+          <View>
+            {
+              conIDs.map((conID, idx) => {
+                return <List.Item
+                  key={idx}
+                  onPress={() => this.changeConID(conID)}
+                  title={conID}
+                  description={conID}
+                  left={props => <List.Icon {...props} icon="folder" />}
+                />
+              })
+            }
+          </View>)
+      }
   }
 }
 
