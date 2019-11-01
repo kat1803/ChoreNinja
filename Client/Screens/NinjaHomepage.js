158f088b7c7e2d74da364abf9719072dd9a3d11d
KennethChong <kenneth.chong@sjsu.edu>
2019-11-01 14:36:07 -0700

    Update some UI information for both Homepage and Ninja

--- Client/Screens/NinjaHomepage.js
+++ Client/Screens/NinjaHomepage.js
@@ -65,17 +65,6 @@ class NinjaHomepage extends React.Component {
                 </View>
                 <Text />
 
-                {/* Job Field */}
-                <View style={{ flexDirection: "row" }}>
-                  <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-                    Field:{" "}
-                  </Text>
-                  <Text style={{ fontSize: 20, fontStyle: "italic" }}>
-                    {post.field}
-                  </Text>
-                </View>
-                <Text />
-
                 {/* Due Date */}
                 <View style={{ flexDirection: "row" }}>
                   <Text style={{ fontSize: 18, fontWeight: "bold" }}>
@@ -87,7 +76,7 @@ class NinjaHomepage extends React.Component {
                 </View>
                 <Text />
 
-                {/* Start Time */}
+                {/* Time */}
                 <View style={{ flexDirection: "row" }}>
                   <Text style={{ fontSize: 18, fontWeight: "bold" }}>
                     Time: between {" "}
@@ -126,7 +115,7 @@ class NinjaHomepage extends React.Component {
                 </View>
                 <Text />
 
-                {/* Payment Method */}
+                {/* Payment Method
                 <View style={{ flexDirection: "row" }}>
                   <Text style={{ fontSize: 18, fontWeight: "bold" }}>
                     Payment Method: {" "}
@@ -135,7 +124,7 @@ class NinjaHomepage extends React.Component {
                     {post.paymentMethod}
                   </Text>
                 </View>
-                <Text />
+                <Text /> */}
 
                 <View
                   style={{
