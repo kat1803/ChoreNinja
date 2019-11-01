5be173cf7626187892a85c1a10c0362447d0bdc7
Hung Tang <hung.tang@sjsu.edu>
2019-11-01 13:10:05 -0700

    update navigation bar

--- Client/Screens/MenuNavBar.js
+++ Client/Screens/MenuNavBar.js
@@ -6,34 +6,42 @@ import {createStackNavigator } from 'react-navigation-stack';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 
 class MenuNavBar extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            signout: false
+        }
+    }
+    // handleSignOut = () => {
+    //     if (signout){
+    //         this.props.signout();
+    //     }
+    // }
     render(){
         return(
             <View style={{ flex:1, flexDirection: "column"}}>
                 <View style={{ flex: 1, justifyContent: 'center', 
                         flexDirection: 'row',
                         borderBottomColor: 'black',
-                        borderBottomWidth: 1, }}>
-                    <Text>Ninja Home</Text>
+                        borderBottomWidth: 1,  }}>
                     <Button
                     title="Ninja Home"
                     onPress={() => this.props.navigation.navigate('Ninja Home')}
                     />
                 </View>
                 
-                <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'row',borderBottomColor: 'black',
+                <View style={{ flex: 1, flexDirection: 'row',borderBottomColor: 'black',
                         borderBottomWidth: 1,  }}>
-                    <Text>Task Screen</Text>
                     <Button
                     title="Task Screen"
                     onPress={() => this.props.navigation.navigate('Task')}
                     />
                 </View>
-                <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'row',borderBottomColor: 'black',
+                <View style={{ flex: 1, flexDirection: 'row',borderBottomColor: 'black',
                         borderBottomWidth: 1,  }}>
-                    <Text>Ninja Bio</Text>
                     <FAIcon
                         name="user"
-                        size={30}
+                        size={60}
                         color='#0091EA'
                         />
                     <Button
@@ -41,18 +49,20 @@ class MenuNavBar extends Component {
                     onPress={() => this.props.navigation.navigate('Ninja')}
                     />
                 </View>
-                <View style={{flex: 1, justifyContent: 'center', flexDirection: 'row',borderBottomColor: 'black',
-                        borderBottomWidth: 1,  }}>
-                    <Text>About Screen</Text>
+                <View style={{flex: 1, flexDirection: 'row',borderBottomColor: 'black',
+                        borderBottomWidth: 1,  }}> 
                     <Button
                     title="About"
                     onPress={() => this.props.navigation.navigate('About')}
                     />
                 </View>
+            
                 
             </View>
         )
     }
 }
+
+
 
 export default MenuNavBar;
--- Client/Screens/NinjaBio.js
+++ Client/Screens/NinjaBio.js
@@ -23,7 +23,7 @@ class NinjaBio extends React.Component {
             phone: "",
             companyName: "",
 
-            editBio: null,
+            editBio: false,
         };
     }
 
@@ -41,154 +41,153 @@ class NinjaBio extends React.Component {
 
     render() {
         return (
-            <ScrollView>
-                <View>
-                {this.state.editBio != null ? (
-                    <Card>
+                <ScrollView>               
+               { this.state.editBio  && 
+                   <View>
+                        <Card>
                         <View>
-                            <Text style={{ fontSize: 20, fontWeight: "bold", textAlign: "center", padding: 10 }}> Edit Your Bio </Text>
-
-                            {/* Company Name */}
-                            <TextInput
-                                style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
-                                underlineColorAndroid="transparent"
-                                numberOfLines={1}
-                                label="CompanyName"
-                                onChangeText={companyName => this.setState({ companyName })}
-                                value={this.state.companyName}
-                            />
-
-                            {/* Description */}
-                            <TextInput
-                                style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
-                                underlineColorAndroid="transparent"
-                                numberOfLines={1}
-                                label="Description"
-                                onChangeText={description => this.setState({ description })}
-                                value={this.state.description}
-                            />
-
-                            {/* Skills */}
-                            <TextInput
-                                style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
-                                underlineColorAndroid="transparent"
-                                numberOfLines={1}
-                                label="Skills"
-                                onChangeText={skills => this.setState.edit({ skills })}
-                                value={this.state.skills}
-                            />
-
-                            {/* Phone */}
-                            <TextInput
-                                style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
-                                underlineColorAndroid="transparent"
-                                numberOfLines={1}
-                                label="Phone"
-                                onChangeText={phone => this.setState({ phone })}
-                                value={this.state.phone}
-                            />
-
-                            {/* Email */}
-                            <TextInput
-                                style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
-                                underlineColorAndroid="transparent"
-                                numberOfLines={1}
-                                label="Email"
-                                onChangeText={email => this.setState({ email })}
-                                value={this.state.email}
-                            />
-
-                            <Button
-                                style={{
-                                    width: 100,
-                                    alignSelf: "center",
-                                    backgroundColor: "#01479b",
-                                    margin: 10
-                                }}
-                                mode="contained"
-                                onPress= {()=> this.handleSave()}
-                            >
-                            <Text>Save</Text>
-                            </Button>
-                        </View>
-                    </Card>
-                ):(
-                    <View>
-                        {this.props.posts && this.props.posts.map((post, idx) => (
-                            <View style={{ flex: 1, marginTop: 20 }}>
-                                <Card
-                                    key={idx}
-                                    //title={post.companyName.trim}
-                                    image={require("../assets/HungcarriesTaiwanFlag.jpeg")}
-                                    containerStyle={{ backgroundColor: '#F5F5F5' }}
-                                >
-                                    <View
-                                        style={{
-                                            borderRadius: 3,
-                                            borderWidth: 1,
-                                            borderColor: "#d6d7da",
-                                            //flex: 3,
-                                            justifyContent: "flex-start",
-                                            alignItems: "flex-start",
-                                            flexDirection: "column",
-                                            padding: 5,
-                                            backgroundColor: '#FFFAFA'
-                                        }}
-                                    >
-                                        {/* <Text style={{ fontWeight: "bold", fontSize: 18 }}>Bio: </Text> */}
-                                        <Text style={{ fontSize: 20, fontStyle: 'italic' }}>
-                                        {post.description}
-                                        </Text>
-                                        <Text></Text>
-
-                                        {/* Ratings */}
-                                        <View style={{ flexDirection: 'row' }}>
-                                            <Text style={{ fontWeight: "bold", fontStyle: 'italic', fontSize: 18 }}>Rating: </Text>
-                                            <Text style={{ fontSize: 18 }}>98% positive</Text>
-                                        </View>
-                                        <Text></Text>
-
-                                        {/* Job Completed */}
-                                        <View style={{ flexDirection: 'row' }}>
-                                            <Text style={{ fontWeight: "bold", fontStyle: 'italic', fontSize: 18 }}>Job Completed: </Text>
-                                            <Text style={{ fontSize: 18 }}>20</Text>
-                                        </View>
-                                        <Text></Text>
-
-                                        {/* Skills */}
-                                        <Text style={{ fontWeight: "bold", fontStyle: 'italic', fontSize: 18 }}>Skills: </Text>
-                                        <Text style={{ fontSize: 18 }}>{post.skills.toString()}</Text>
-                                        <Text></Text>
-
-                                        {/* Contact Information */}
-                                        <Text style={{ fontWeight: "bold", fontStyle: 'italic', fontSize: 18 }}>Contact Info: </Text>
-                                        {/* Phone Number */}
-                                        <View style={{ flexDirection: 'row' }}>
-                                            <Text style={{ fontWeight: 'bold', fontSize: 17 }}> Tel: </Text>
-                                            <Text style={{ fontSize: 18 }}> {post.phone.toString()} </Text>
-                                        </View>
-                                        {/* Email */}
-                                        <View style={{ flexDirection: 'row' }}>
-                                            <Text style={{ fontWeight: 'bold', fontSize: 17 }}> Email: </Text>
-                                            <Text style={{ fontSize: 18 }}> {post.email.toString()} </Text>
-                                        </View>
-                                        <Text></Text>
-
-                                    </View>
-                                    <Button
-                                        style={{ width: 100, alignSelf: 'center', margin: 10 }}
-                                        mode="contained"
-                                        onPress={() => this.setState({ editBio: true, idx})}
-                                    >
-                                    <Text>Edit Bio</Text>
-                                    </Button>
-                                </Card>
+                                <Text style={{ fontSize: 20, fontWeight: "bold", textAlign: "center", padding: 10 }}>Edit Your Bio</Text>
+                                {/* Company Name */}
+                                <TextInput
+                                    style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
+                                    underlineColorAndroid="transparent"
+                                    numberOfLines={1}
+                                    label="CompanyName"
+                                    onChangeText={companyName => this.setState({ companyName })}
+                                    value={this.state.companyName}
+                                />
+                                {/* Description */}
+                                <TextInput
+                                    style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
+                                    underlineColorAndroid="transparent"
+                                    numberOfLines={1}
+                                    label="Description"
+                                    onChangeText={description => this.setState({ description })}
+                                    value={this.state.description}
+                                />
+                                {/* Skills */}
+                                <TextInput
+                                    style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
+                                    underlineColorAndroid="transparent"
+                                    numberOfLines={1}
+                                    label="Skills"
+                                    onChangeText={skills => this.setState({ skills })}
+                                    value={this.state.skills}
+                                />
+                                {/* Phone */}
+                                <TextInput
+                                    style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
+                                    underlineColorAndroid="transparent"
+                                    numberOfLines={1}
+                                    label="Phone"
+                                    onChangeText={phone => this.setState({ phone })}
+                                    value={this.state.phone}
+                                />
+                                {/* Email */}
+                                <TextInput
+                                    style={{ backgroundColor: "rgb(250,250,250)", padding: 10 }}
+                                    underlineColorAndroid="transparent"
+                                    numberOfLines={1}
+                                    label="Email"
+                                    onChangeText={email => this.setState({ email })}
+                                    value={this.state.email}
+                                />
+                                <Button
+                                    style={{
+                                        width: 100,
+                                        alignSelf: "center",
+                                        backgroundColor: "#01479b",
+                                        margin: 10
+                                    }}
+                                    mode="contained"
+                                    onPress= {()=> this.handleSave()}
+                                    title="Save"
+                                />
+                                {/* <Text>Save</Text>
+                                </Button> */}
                             </View>
-                        ))}
-                    </View>
-                )};
+                        </Card>
+                   </View>
+               }
+               {
+                    !this.state.editBio  && 
+                    <View>
+                    {this.props.posts && this.props.posts.map((post, idx) => (
+                        <View style={{ flex: 1, marginTop: 20 }}>
+                            <Card
+                                key={idx}
+                                //title={post.companyName.trim}
+                                image={require("../assets/HungcarriesTaiwanFlag.jpeg")}
+                                containerStyle={{ backgroundColor: '#F5F5F5' }}
+                            >
+                                <View
+                                    style={{
+                                        borderRadius: 3,
+                                        borderWidth: 1,
+                                        borderColor: "#d6d7da",
+                                        //flex: 3,
+                                        justifyContent: "flex-start",
+                                        alignItems: "flex-start",
+                                        flexDirection: "column",
+                                        padding: 5,
+                                        backgroundColor: '#FFFAFA'
+                                    }}
+                                >
+                                    {/* <Text style={{ fontWeight: "bold", fontSize: 18 }}>Bio: </Text> */}
+                                    <Text style={{ fontSize: 20, fontStyle: 'italic' }}>{post.description}</Text>
+                                   
+
+                                    {/* Ratings */}
+                                    <View style={{ flexDirection: 'row' }}>
+                                        <Text style={{ fontWeight: "bold", fontStyle: 'italic', fontSize: 18 }}>Rating:</Text>
+                                        <Text style={{ fontSize: 18 }}>98% positive</Text>
+                                    </View>
+                                  
+
+                                    {/* Job Completed */}
+                                    <View style={{ flexDirection: 'row' }}>
+                                        <Text style={{ fontWeight: "bold", fontStyle: 'italic', fontSize: 18 }}>Job Completed:</Text>
+                                        <Text style={{ fontSize: 18 }}>20</Text>
+                                    </View>
+                                    
+
+                                    {/* Skills */}
+                                    <Text style={{ fontWeight: "bold", fontStyle: 'italic', fontSize: 18 }}>Skills:</Text>
+                                    <Text style={{ fontSize: 18 }}>{post.skills.toString()}</Text>
+                         
+
+                                    {/* Contact Information */}
+                                    <Text style={{ fontWeight: "bold", fontStyle: 'italic', fontSize: 18 }}>Contact Info:</Text>
+                                    {/* Phone Number */}
+                                    <View style={{ flexDirection: 'row' }}>
+                                        <Text style={{ fontWeight: 'bold', fontSize: 17 }}> Tel:</Text>
+                                        <Text style={{ fontSize: 18 }}>{post.phone.toString()}</Text>
+                                    </View>
+                                    {/* Email */}
+                                    <View style={{ flexDirection: 'row' }}>
+                                        <Text style={{ fontWeight: 'bold', fontSize: 17 }}> Email: </Text>
+                                        <Text style={{ fontSize: 18 }}> {post.email.toString()} </Text>
+                                    </View>
+                                   
+
+                                </View>
+                                <Button
+                                    style={{ width: 100, alignSelf: 'center', margin: 10 }}
+                                    mode="contained"
+                                    onPress={() => this.setState({ editBio: true, idx})}
+                                >
+                                <Text>Edit Bio</Text>
+                                </Button>
+                            </Card>
+                        </View>
+                    ))}
                 </View>
-            </ScrollView>
+
+               }
+                             )};
+         
+        </ScrollView>
+            
         );
     }
 }
